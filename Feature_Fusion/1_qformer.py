ca99f9e9f684d9e61e4a29e752b2ca657add2f9b
bupt-yiwo <81768934+bupt-yiwo@users.noreply.github.com>
2025-07-26 10:47:14 +0800

    Delete Feature_Fusion directory

--- Feature_Fusion/1_qformer.py
+++
@@ -1,7 +0,0 @@
-import torch
-from qformer import QFormer  
-
-qformer = QFormer()
-image_feats = torch.randn(2, 256, 768)  # e.g. from ViT
-query_out = qformer(image_feats)       # [2, 32, 768]
-print(query_out)
